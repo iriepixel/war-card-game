a7a943cf458ca7785d07b76e84455e9a7c21ab8f
Jev Smolnikov <jev@iriepixel.com>
2024-01-08 22:26:06 +0000

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Jev Smolnikov on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
